3c8a2852e98a35916b5adabdcd0b6b2e0a88f858
Abhiman Yadav <abhimanyadav@Abhimans-MacBook-Pro.local>
2021-01-24 12:50:01 +0800

    save

--- server/paths.py
+++ server/paths.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from invoiceAI import getText
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
@@ -6,13 +7,26 @@ app = Flask(__name__)
 def index():
     return 'Server is up and running!'
 
+"""
+Ignore this one.
+"""
 @app.route('/setTemplate', methods = ['POST'])
 def callSetTemplate():
     pass
 
 @app.route('/parseInvoice', methods = ['POST'])
 def callParseInvoice():
-    pass
+    uploaded_file = request.files['file']
+    template = "temp"
+
+    if uploaded_file.filename != '':
+        res = getText(uploaded_file, template)
+        result = { res }
+        return jsonify(result)
+    else:
+        result = { "response": "error" }
+        return jsonify(result)
+
 
 @app.route('/removeInvoice', methods = ['DELETE'])
 def callRemoveInvoice():
